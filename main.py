f63afd300ac0ee250ffaa8442acb1858db6c9b0c
kubix23 <kubix2770@gmail.com>
2023-09-21 01:53:55 +0200

    Working plot, test on RSI

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
 import matplotlib
-import pandas_datareader as web
-import pandas as pd
-import talib
 import mplfinance as fplt
+import pandas_datareader as web
+import talib
+
 matplotlib.use("TkAgg")
 
 #web.DataReader('^DJI', 'stooq')
 #pd.read_csv("./resources/akcje.csv", delimiter='\t', header=None)
 if __name__ == '__main__':
-    lista = web.DataReader('^DJI', 'stooq')[::-1]
+
+    lista = web.DataReader('KOM.PL', data_source='stooq', start="2010-01-01")[::-1]
     lista["RSI"] = talib.RSI(lista["Close"])
     print(lista)
-    lista = lista.tail(30)
-    rsi = fplt.make_addplot(lista["RSI"],secondary_y=True)
+    lista = lista
 
-    chart = fplt.figure(style='charles',figsize=(7,8))
-    ax1 = chart.add_subplot(2,1,1)
-    ax2 = chart.add_subplot(2,1,2)
+    chart = fplt.figure(style='charles',figsize=(6,7))
+    ax1 = chart.add_subplot(3,1,(1,2))
+    ax2 = chart.add_subplot(3,1,3, sharex=ax1)
+    ap = [
+        fplt.make_addplot(lista["RSI"], ax=ax2),
+        fplt.make_addplot([20]*len(lista), ax=ax2),
+        fplt.make_addplot([80]*len(lista), ax=ax2),
+    ]
     fplt.plot(
         lista,
         type="candle",
         ax = ax1,
-        volume=ax2
+        addplot= ap
     )
     fplt.show()
